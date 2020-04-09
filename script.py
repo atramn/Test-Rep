e69390edd214c01b5c2222d7db5510cf1071f4d3
Atramn Kamson <atramn@Atramns-MacBook-Pro.local>
2020-04-09 01:17:50 -0400

    Remove some Code

--- script.py
+++ script.py
@@ -1,25 +1,5 @@
-import sys
-from os import rename
 import requests
-import math
-
-# # print(sys.version)
-# print(sys.executable)
 
 
-# def greet(who_to_greet):
-#     test = "Test"
-#     greeting = "Hello, {}".format(who_to_greet)
-#     return greeting
-
-
-# # print("Hello")
-# # print("Atramn")
-
-# r = requests.get("https://coreyms.com")
-# print(r.status_code)
-
-# input in vscode without command line
-
-name = input("You name?")
-print("Hello, ", name)
+r = requests.get("https://coreyms.com")
+print(r.status_code)
